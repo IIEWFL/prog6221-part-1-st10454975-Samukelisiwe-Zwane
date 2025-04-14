b2c4c2895ecb0b0a28cd24da3686b6f30060ff23
unknown <ST10454975@vcconnect.eu.za>
2025-04-14 10:20:10 +0200

    made the chat bot voice line

---
+++ ProgAssignPart1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.IO;
+using System.Reflection;
+
+
+namespace ProgAssignPart1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string audio = "ProgAssignPart1.greeting audio.wav";
+                Assembly currentAssembly = Assembly.GetExecutingAssembly();
+
+                using (Stream stream = currentAssembly.GetManifestResourceStream(audio))
+                {
+                    if(stream != null)
+                    {
+                        using(SoundPlayer player = new SoundPlayer(stream))
+                        {
+                            Console.WriteLine($"Playing greeting: {audio}");
+                            player.PlaySync();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: audio '{audio}' was not found");
+                    }
+
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"error : {ex.Message}");
+
+
+            }
+
+           
+            
+        }
+    }
+}
