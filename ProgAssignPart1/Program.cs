15aaae294259a01331cdfad260d2f1f910b4fc30
unknown <ST10454975@vcconnect.eu.za>
2025-04-21 15:05:29 +0200

    used vs formating option to format document

--- ProgAssignPart1/Program.cs
+++ ProgAssignPart1/Program.cs
@@ -10,7 +10,6 @@ using System.Reflection;
 
 namespace ProgAssignPart1
 {
-    
     public class Program
     {
         public static string GetBasicResponse(string question)
@@ -20,22 +19,33 @@ namespace ProgAssignPart1
             switch (changeStringToLowerCase)
             {
                 case "how are you":
+
                     Console.ForegroundColor = ConsoleColor.DarkCyan;
+
                     string howAreYouResponse = "As your cyber security assistant bot, i am operating at optimal performance so that i can help you stay safe online";
-                    
+
                     return howAreYouResponse;
-                    
+
                 case "what is your purpose":
+
                     Console.ForegroundColor = ConsoleColor.DarkCyan;
+
                     string purposeResponse = "My purpose is to help you with knowledge regarding cybersecurity so that you can protect yourself form cyber theats";
-                    
+
                     return purposeResponse;
+
                 case "what can i ask about you":
+
                     Console.ForegroundColor = ConsoleColor.DarkCyan;
+
                     Console.Write("you can ask me anything related to staying safe online! here are some key areas i can assist you with:\n");
+
                     Console.ForegroundColor = ConsoleColor.Yellow;
+
                     Console.WriteLine("------------------------------------------------------------------------------------------------------");
+
                     Console.ForegroundColor = ConsoleColor.Cyan;
+
                     string askAboutResponse = "You can ask me anything related to staying safe online. I can assist you with the follwing: \n" +
                         "Creating a strong and unique password \n " +
                         "Identifying and avoiding phishing attemps \n"
@@ -43,20 +53,31 @@ namespace ProgAssignPart1
                         "preventing malware infections \n" +
                         "Portecing of personal information \n" +
                         "safe browsing practice";
+
                     Console.Write(askAboutResponse);
+
                     Console.ForegroundColor = ConsoleColor.Yellow;
+
                     Console.WriteLine("");
+
                     Console.WriteLine("----------------------------------------------------------------------------------------------------");
+
                     Console.ForegroundColor = ConsoleColor.Cyan;
+
                     Console.Write("this is a general idea of what you can ask me");
+
                     Console.ResetColor();
 
                     return "";
 
                 default:
+
                     Console.ForegroundColor = ConsoleColor.Cyan;
+
                     string defaultResponse = "sorry but i dont quite understand that could you rephrase";
+
                     Console.ResetColor();
+
                     return defaultResponse;
             }
         }
@@ -75,20 +96,20 @@ namespace ProgAssignPart1
 /_/   \_\_/\_/ \__,_|_|  \___|_| |_|\___||___/___/ |_.__/ \___/ \__|
 ";
 
-
             Console.WriteLine(asciiArt);
+
             try
             {
                 string audio = "ProgAssignPart1.greeting audio.wav";
+
                 Assembly currentAssembly = Assembly.GetExecutingAssembly();
 
                 using (Stream stream = currentAssembly.GetManifestResourceStream(audio))
                 {
-                    if(stream != null)
+                    if (stream != null)
                     {
-                        using(SoundPlayer player = new SoundPlayer(stream))
+                        using (SoundPlayer player = new SoundPlayer(stream))
                         {
-                            
                             player.PlaySync();
                         }
                     }
@@ -100,28 +121,29 @@ namespace ProgAssignPart1
                 }
 
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine($"error : {ex.Message}");
-
-
             }
 
             Console.WriteLine("");
+
             Console.WriteLine("");
-           
+
             Console.WriteLine("What is your name");
 
             Console.WriteLine("");
 
             var name = Console.ReadLine();
 
-            Console.WriteLine("Greetings " + name + " how can i help you stay safer online "+ "(enter exit to close program");
+            Console.WriteLine("Greetings " + name + " how can i help you stay safer online " + "(enter exit to close program");
 
             string input;
+
             do
             {
                 Console.ForegroundColor = ConsoleColor.Red;
+
                 Console.WriteLine(" ");
 
                 input = Console.ReadLine();
@@ -129,34 +151,27 @@ namespace ProgAssignPart1
                 if (input?.ToLower() == "exit")
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
+
                     Console.WriteLine("stay safe online, goodbye");
+
                     break;
                 }
                 if (!string.IsNullOrEmpty(input))
                 {
-                   
                     Console.WriteLine(GetBasicResponse(input));
-
-
                 }
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
+
                     Console.WriteLine("please enter your cyber security question.");
                 }
                 Console.ForegroundColor = ConsoleColor.Red;
+
                 Console.WriteLine("please enter your cyber security question");
 
             } while (true);
-
-
-
-
-
-
-
-
         }
-       
+
     }
 }
